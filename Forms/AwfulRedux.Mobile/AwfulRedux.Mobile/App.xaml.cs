476bf7694db6d8c07d7dc5526ab31f09712dc19f
Tim Miller <innerlogic4321@gmail.com>
2016-05-10 11:43:34 -0400

    Add basic Xamarin Forms Prism support

---
+++ Forms/AwfulRedux.Mobile/AwfulRedux.Mobile/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Unity;
+using AwfulRedux.Mobile.Views;
+
+namespace AwfulRedux.Mobile
+{
+    public partial class App : PrismApplication
+    {
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
